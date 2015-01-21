ce6ed0b191ff0771782ebbdf279dab31ff343a6b
abr314 <abr314@icloud.com>
2015-01-21 18:07:51 -0500

    Initial Commit

---
+++ asyncpractice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  asyncpractice
+//
+//  Created by Abraham Brovold on 1/21/15.
+//  Copyright (c) 2015 Abraham Brovold. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ asyncpractice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  asyncpractice
+//
+//  Created by Abraham Brovold on 1/21/15.
+//  Copyright (c) 2015 Abraham Brovold. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
